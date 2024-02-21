5db5d1e3503d5fad35b911db42fb54f41b6e56dc
YoyoBLemail <yoelbl2@gmail.com>
2024-02-21 21:00:16 +0200

    CRUD complete

--- src/jsx/components/table/OrganizationsCRM.jsx
+++ src/jsx/components/table/OrganizationsCRM.jsx
@@ -6,7 +6,7 @@ import { useFormik } from "formik";
 
 import { Input } from "../creatdComponents/Input";
 import { createUser, deleteUser, updateUser } from "../../../lib/userCRUD";
-import { YupUserSchema } from "../../../lib/YupMemberSchema";
+import { YupUserSchema, yupOrganizationSchema } from "../../../lib/YupSchemas";
 import SortingTH from "../creatdComponents/SortingTH";
 import SearchByInput from "../creatdComponents/SeachByInput";
 import { useSearchParams } from "react-router-dom";
@@ -19,6 +19,7 @@ const organizationsKeysObj = [
    "organizationEmail",
    "organizationMessageBody",
    "organizationType",
+   "status",
 ];
 const fields = {
    organizationName: "Organization name",
@@ -27,6 +28,7 @@ const fields = {
    organizationEmail: "Email",
    organizationMessageBody: "Message",
    organizationType: "Organization Type",
+   status: "Status",
 };
 
 const OrganizationsCRM = () => {
@@ -36,47 +38,64 @@ const OrganizationsCRM = () => {
    // const sortOrder = searchParams.get("sortOrder");
 
    useEffect(() => {
-      getOrganizations();
+      fetchOrganizations();
    }, []);
 
    const API_URI = "https://kibbutzil.online/organizations";
 
-   async function getOrganizations() {
+   async function fetchOrganizations() {
       const res = await axios.get(API_URI);
       setOrganizations(res.data);
    }
 
-   async function searchUser(search) {
-      const { searchBy, searchValue } = search;
-      try {
-         const user = await axios.post(API_URI, {
-            [searchBy]: searchValue,
-         });
-         setOrganizations(user.data);
-      } catch (error) {
-         console.log(error);
-      }
-   }
-
-   // async function handleDelete(user) {
+   // async function searchUser(search) {
+   //    const { searchBy, searchValue } = search;
    //    try {
-   //       await deleteUser(API_URI, user._id);
-   //       fetchUsers();
-   //       swal("OK", "Successfully deleted", "success");
+   //       const user = await axios.post(API_URI, {
+   //          [searchBy]: searchValue,
+   //       });
+   //       setOrganizations(user.data);
    //    } catch (error) {
-   //       swal("Oops", "Something went wrong!", "error");
    //       console.log(error);
    //    }
    // }
+
+   async function handleDelete(id) {
+      try {
+         await axios.delete(`${API_URI}/${id}`);
+         fetchOrganizations();
+         swal("OK", "Successfully deleted", "success");
+      } catch (error) {
+         swal("Oops", "Something went wrong!", "error");
+         console.log(error);
+      }
+   }
 
    //User table row Component
    const OrganizationTr = ({ organization = {}, index = 1 }) => {
       // if (!user.fullName) return;
       return (
          <tr>
-            {organizationsKeysObj.map((key) => (
-               <td className="py-2">{organization[key]}</td>
-            ))}
+            <td>{index + 1}</td>
+            {organizationsKeysObj.map((key) => {
+               if (key === "status") {
+                  const value = organization[key];
+                  return (
+                     <td className="py-2 text-center" key={organization[key]}>
+                        {value === "active" ? (
+                           <i class="bi bi-circle-fill text-success fs-4"></i>
+                        ) : (
+                           <i class="bi bi-circle-fill text-danger fs-4"></i>
+                        )}
+                     </td>
+                  );
+               }
+               return (
+                  <td className="py-2" key={organization[key]}>
+                     {organization[key]}
+                  </td>
+               );
+            })}
 
             {/*CRM  */}
             <td className="py-2 ">
@@ -85,14 +104,14 @@ const OrganizationsCRM = () => {
                      className="me-2 btn-sm"
                      variant="warning btn-rounded"
                      onClick={() => {
-                        // form.setValues(user);
-                        // setBasicModal(true);
+                        form.setValues(organization);
+                        setBasicModal(true);
                      }}
                   >
                      <i className="bi bi-pencil-square fs-5"></i>
                   </Button>
                   <Button
-                     onClick={() => {}}
+                     onClick={() => handleDelete(organization._id)}
                      className="me-2 btn-sm"
                      variant="primary btn-rounded"
                   >
@@ -105,54 +124,43 @@ const OrganizationsCRM = () => {
    };
 
    //Formik
-   /*
-   const YupNewVolunteerSchema = YupUserSchema().pick([
-      "fullName",
-      "email",
-      "location",
-      "phoneNumber",
-      "gender",
-      "positionAntilNow",
-      "fecerPosition",
-      "yearExperience",
-      "linkdinURL",
-   ]);
 
    const form = useFormik({
       validateOnMount: true,
-
+      abortEarly: false,
       initialValues: {
-         fullName: "",
-         email: "",
-         location: "",
-         phoneNumber: "",
-         gender: "",
-         positionAntilNow: "",
-         fecerPosition: "",
-         yearExperience: "",
-         linkdinURL: "",
+         organizationName: "",
+         organizationPhoneNumber: "",
+         organizationContactName: "",
+         organizationEmail: "",
+         organizationMessageBody: "",
+         organizationType: "",
+         status: "active",
       },
-      validationSchema: YupNewVolunteerSchema,
+      validationSchema: yupOrganizationSchema(),
 
       async onSubmit(values) {
-         const processedValues = YupNewVolunteerSchema.validateSync(values);
+         const processedValues = await yupOrganizationSchema().validate(values);
          try {
             if (form.values._id) {
-               await updateUser(API_URI, form.values._id, processedValues);
+               await axios.put(
+                  `${API_URI}/${form.values._id}`,
+                  processedValues
+               );
                swal("OK", "Successfully edited", "success");
             } else {
-               await createUser(API_URI, processedValues);
+               const res = await axios.post(API_URI, processedValues);
                swal("OK", "Successfully added", "success");
             }
             form.resetForm();
-            getOrganizations();
+            setBasicModal(false);
+            fetchOrganizations();
          } catch (error) {
             swal("Oops", "Something went wrong!", "error");
             console.log(error);
          }
       },
    });
-   */
 
    return (
       <Fragment>
@@ -180,15 +188,17 @@ const OrganizationsCRM = () => {
                      <Table responsive hover>
                         <thead>
                            <tr>
+                              <th>#</th>
                               {organizationsKeysObj.map((key) => (
-                                 <th>{fields[key]}</th>
+                                 <th key={fields[key]}>{fields[key]}</th>
                               ))}
+                              <th>CRM</th>
                            </tr>
                         </thead>
                         <tbody>
                            {organizations.map((organization, index) => (
                               <OrganizationTr
-                                 key={index}
+                                 key={organization}
                                  organization={organization}
                                  index={index}
                               />
@@ -209,7 +219,8 @@ const OrganizationsCRM = () => {
                 modal-dialog-scrollable"
          >
             <Modal.Header>
-               <Modal.Title>Modal title</Modal.Title>
+               <Modal.Title>New Organization</Modal.Title>
+
                <Button
                   variant=""
                   className="btn-close"
@@ -221,28 +232,72 @@ const OrganizationsCRM = () => {
             </Modal.Header>
             <Modal.Body>
                <div className="basic-form">
-                  <form></form>
+                  <form onSubmit={form.handleSubmit}>
+                     {organizationsKeysObj.map((key) => {
+                        if (key === "status") {
+                           return (
+                              <div className="form-group mb-3 row">
+                                 <label
+                                    htmlFor="status"
+                                    className="col-sm-3 col-form-label"
+                                 >
+                                    Status
+                                 </label>
+                                 <div className="col-sm-9">
+                                    <select
+                                       className="form-select"
+                                       style={{ height: "3rem" }}
+                                       id="status"
+                                       name="status"
+                                       value={form.values.status}
+                                       onChange={form.handleChange}
+                                    >
+                                       <option value="active" defaultValue>
+                                          Active
+                                       </option>
+                                       <option value="inactive">
+                                          Inactive
+                                       </option>
+                                    </select>
+                                 </div>
+                              </div>
+                           );
+                        }
+                        return (
+                           <Input
+                              label={fields[key]}
+                              error={form.touched[key] && form.errors[key]}
+                              {...form.getFieldProps(key)}
+                              key={key}
+                              required={
+                                 key === "organizationType" ? false : true
+                              }
+                           />
+                        );
+                     })}
+                     {/* role */}
+                  </form>
                </div>
             </Modal.Body>
             <Modal.Footer>
                <Button
                   onClick={() => {
-                     // setBasicModal(false);
-                     // form.resetForm();
+                     setBasicModal(false);
+                     form.resetForm();
                   }}
+                  type="reset"
                   variant="danger light"
+                  size="lg"
                >
                   Close
                </Button>
                <Button
-                  onClick={() => {
-                     // form.handleSubmit();
-                     // setBasicModal(false);
-                  }}
+                  type="submit"
                   variant="primary"
+                  size="lg"
+                  onClick={form.handleSubmit}
                >
-                  Send
-                  {/* {form.values._id ? "Edit member" : "Add member"} */}
+                  {form.values._id ? "Edit" : "Add"}
                </Button>
             </Modal.Footer>
          </Modal>
